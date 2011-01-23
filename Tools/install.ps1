33bee38da3fec27788bde271436e011542e141b3
Dan Mohl <dmohl@yahoo.com>
2011-01-23 14:08:10 -0600

    .

--- Tools/install.ps1
+++ Tools/install.ps1
@@ -4,7 +4,7 @@ try {
   $project.Object.References.Add("System.Data.Entity"); 
 }
 catch {
-  # This is hack to get around "a recent regression in the default branch". This will be removed in a future version. See http://nuget.codeplex.com/Thread/View.aspx?ThreadId=240992 for more information.
+  # This is a hack to get around a problem that currently occurs when trying to add these references to the F# project. This will be removed in a future version. See http://nuget.codeplex.com/Thread/View.aspx?ThreadId=240992 for more information.
   $project.Object.References.Add("System.Data.HACK"); 
   $project.Object.References.Add("System.Data.Entity.HACK"); 
 }
--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 c:\nuget\nuget.exe pack c:\git\FSRepository\FSRepository.nuspec -b c:\git\FSRepository -o c:\git\FSRepository
+copy c:\git\FSRepository\*.* c:\nuget /Y
 pause
