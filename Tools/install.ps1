af2b6b3227b5e7b0af7c3ed59cc9bfbf6b7da260
Dan Mohl <dmohl@yahoo.com>
2011-01-23 14:59:31 -0600

    changed the install.ps1

--- Tools/install.ps1
+++ Tools/install.ps1
@@ -1,10 +1,5 @@
 param($installPath, $toolsPath, $package, $project)
-try {
-  $project.Object.References.Add("System.Data"); 
-  $project.Object.References.Add("System.Data.Entity"); 
-}
-catch {
-  # This is a hack to get around a problem that currently occurs when trying to add these references to the F# project. This will be removed in a future version. See http://nuget.codeplex.com/Thread/View.aspx?ThreadId=240992 for more information.
-  $project.Object.References.Add("System.Data.HACK"); 
-  $project.Object.References.Add("System.Data.Entity.HACK"); 
+if($project.Type -eq 'F#') {
+    $project.Object.References.Add("*System.Data");
+    $project.Object.References.Add("*System.Data.Entity");
 }
