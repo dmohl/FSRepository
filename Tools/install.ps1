bf78b121f8804c71e3c75b8883a0ba6f0afc3ddf
Dan Mohl <dmohl@yahoo.com>
2011-01-23 20:26:27 -0600

    Made a few more modifications to install.ps1

--- Tools/install.ps1
+++ Tools/install.ps1
@@ -1,5 +1,3 @@
 param($installPath, $toolsPath, $package, $project)
-if($project.Type -eq 'F#') {
     $project.Object.References.Add("*System.Data");
     $project.Object.References.Add("*System.Data.Entity");
-}
--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 c:\nuget\nuget.exe pack c:\git\FSRepository\FSRepository.nuspec -b c:\git\FSRepository -o c:\git\FSRepository
-copy c:\git\FSRepository\*.* c:\nuget /Y
+copy c:\git\FSRepository\*.* c:\nuget\FSRepository /Y
 pause
