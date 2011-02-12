66fab416b83ecce4d457cc79bd4ec1d14b15bf7f
Dan Mohl <dmohl@yahoo.com>
2011-02-12 11:02:14 -0600

    Moved all the sample code into a single fs file to avoid the random file add nature of NuGet

--- Content/IRepository.fs.pp
+++
@@ -1,8 +0,0 @@
-﻿namespace $rootnamespace$.Repositories
-
-type IRepository<'a> =
-    abstract Add : 'a -> unit
-    abstract Delete : 'a -> unit
-    abstract GetAll : unit -> seq<'a>
-    abstract GetById : 'b -> 'a
-    abstract Save : unit -> unit
